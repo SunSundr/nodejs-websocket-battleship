75ef350f5b86e6759107ac230596bd37087db034
SunSundr <art.dizigner@gmail.com>
2024-10-24 17:32:32 +0400

    chore: add base configs

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,17 +1,20 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as http from 'http';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as http from 'node:http';
 
-export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+export const httpServer = http.createServer((req, res) => {
+  const __dirname = path.resolve(path.dirname(''));
+  const filePath = path.join(__dirname, req.url === '/' ? 'front/index.html' : `front${req.url}`);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+
+      return;
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
 });
